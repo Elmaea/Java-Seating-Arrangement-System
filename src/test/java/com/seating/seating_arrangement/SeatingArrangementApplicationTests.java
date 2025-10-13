4c0f94ad3250d14fecfb727712656959859d0e90
Elmaea <elma.anish06@gmail.com>
2025-10-13 14:30:00 +0530

    Add Spring Boot backend + frontend pages

---
+++ src/test/java/com/seating/seating_arrangement/SeatingArrangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seating.seating_arrangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatingArrangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
